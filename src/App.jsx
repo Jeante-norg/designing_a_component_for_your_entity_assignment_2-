f73f5a62c59188958602f647e41f2c86e08e4141
Tej <jupitersorbeet@gmail.com>
2025-01-21 12:34:37 +0530

    assignment 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
 
-import Productcard from './components/productcard.jsx';
+import Productcard from "./components/Productcard.jsx";
 
 function App() {
   const cards = Array.from({ length: 10 });
   const gridStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "16px",
+    padding: "16px",
   };
 
   return (
@@ -19,6 +19,7 @@ function App() {
       {cards.map((_, index) => (
         <Productcard key={index} />
       ))}
+      {/* <h1>Hello there</h1> */}
     </div>
   );
 }
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,45 @@
 // write product card here
+// import React from "react";
+import Button from "./button";
+const productImage = "https://picsum.photos/200";
+const name = "Scenic Frame";
+const price = "$49.99";
+
+const Productcard = () => {
+  return (
+    <div style={styles.card}>
+      <img src={productImage} alt="" style={styles.image} />
+      <h2 style={styles.name}>{name}</h2>
+      <p style={styles.price}>Price: {price}</p>
+      <Button />
+    </div>
+  );
+};
+
+const styles = {
+  image: {
+    width: "200px",
+    height: "120px",
+    borderRadius: "10px",
+    borderWidth: "20px",
+  },
+
+  card: {
+    border: "1px white",
+    borderRadius: "9px",
+    padding: "15px",
+    backgroundColor: "#F4D793",
+  },
+
+  name: {
+    color: "#09122C",
+    fontSize: "1.5em",
+  },
+
+  price: {
+    color: "#09122C",
+    fontSize: "1em",
+  },
+};
+
+export default Productcard;
